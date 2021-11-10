9aa1fd8a438260789a1c560cbd59514b76c74167
nutjob <stephan.craane@gmail.com>
2021-11-10 09:42:10 +0100

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+case $1 in
+        "")
+                echo "Need folder name to update! Or use ALL to update all"
+                exit 0
+                ;;
+        ALL|all)
+                for entry in */
+                do
+                        echo "Running update on $entry:"
+                        set -x
+                        docker-compose -f "$entry" pull
+                        docker-compose -f "$entry" up -d --remove-orphans
+                done
+                docker image prune -f
+                exit 0
+                ;;
+        *)
+                if [ -d $1 ]; then
+                        echo "Running update on $1:"
+                        set -x
+                        docker-compose -f "$1" pull
+                        docker-compose -f "$1" up -d --remove-orphans
+                        docker image prune -f
+                        exit 0
+                else
+                        echo "Folder $1 does not exist. Exiting!"
+                        exit 0
+                fi
+                ;;
